436e7d76718c09560ff7bd27d7b35dab8d776a7e
Ary Sugiarto <arysugiarto10@gmail.com>
2020-04-11 18:28:48 +0700

    SwipeRefresh

--- app/src/main/java/id/arysugiarto/beritaku/adapter/ArticleAdapter.kt
+++ app/src/main/java/id/arysugiarto/beritaku/adapter/ArticleAdapter.kt
@@ -1,4 +1,73 @@
 package id.arysugiarto.beritaku.adapter
 
-class ArticleAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import id.arysugiarto.beritaku.R
+import id.arysugiarto.beritaku.model.Article
+import kotlinx.android.synthetic.main.item_article.view.*
+
+class ArticleAdapter (
+    private var articleList: ArrayList<Article>
+): RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>(){
+
+    private val placeHolderImage="https://pbs.twimg.com/profile_images/467502291415617536/SP8_ylk9.png"
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ArticleViewHolder {
+        val itemView: View = LayoutInflater.from(parent.context).inflate(R.layout.item_article, parent,false)
+        return ArticleViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return articleList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleAdapter.ArticleViewHolder, position: Int) {
+        val article: Article = articleList.get(position)
+
+    }
+
+    private fun setPropertiesForArticleViewHolder(articleViewHolder: ArticleViewHolder, article: Article) {
+        checkForUrlToImage(article, articleViewHolder)
+        articleViewHolder.title.text = article?.title
+        articleViewHolder.description.text = article?.description
+    }
+
+    private fun checkForUrlToImage(article: Article, articleViewHolder: ArticleViewHolder) {
+        if (article.urlToImage == null || article.urlToImage.isEmpty()) {
+            Picasso.get()
+                .load(placeHolderImage)
+                .centerCrop()
+                .fit()
+                .into(articleViewHolder.urlToImage)
+        } else {
+            Picasso.get()
+                .load(article.urlToImage)
+                .centerCrop()
+                .fit()
+                .into(articleViewHolder.urlToImage)
+        }
+    }
+
+    fun setArticles(articles: ArrayList<Article>) {
+        articleList = articles
+        notifyDataSetChanged()
+    }
+
+    inner class ArticleViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        val cardView: CardView by lazy { view.cardview }
+        val urlToImage: ImageView by lazy { view.imageNews }
+        val title: TextView by lazy { view.titleNews }
+        val description: TextView by lazy { view.descriptionNews }
+    }
 }
--- app/src/main/java/id/arysugiarto/beritaku/ui/MainActivity.kt
+++ app/src/main/java/id/arysugiarto/beritaku/ui/MainActivity.kt
@@ -2,12 +2,41 @@ package id.arysugiarto.beritaku.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import id.arysugiarto.beritaku.R
+import id.arysugiarto.beritaku.adapter.ArticleAdapter
+import id.arysugiarto.beritaku.api.TopHeadlinesEndpoint
+import id.arysugiarto.beritaku.model.Article
+import id.arysugiarto.beritaku.model.TopHeadlines
+import io.reactivex.disposables.CompositeDisposable
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
+
+    private val ENPOINT_URL by lazy { "https://newsapi.org/v2/" }
+    private lateinit var topHeadlinesEndpoint: TopHeadlinesEndpoint
+    private lateinit var newsApiConfig: String
+    private lateinit var articleAdapter: ArticleAdapter
+    private lateinit var articleList: ArrayList<Article>
+    private lateinit var userKeywordInput: String
+
+//    rx
+    private lateinit var topHeadlinesObservable: Observable <TopHeadlines>
+    private lateinit var compositeDisposable: CompositeDisposable
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+    }
+
+    override fun onRefresh() {
+        TODO("not implemented yet") // Will be implemented at the end
     }
 }
